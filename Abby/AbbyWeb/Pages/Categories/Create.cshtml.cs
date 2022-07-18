8e8a39ffeb07051d49fe3d20dfb033e56e914041
mmraihan <mm_raihan@live.com>
2022-07-18 14:36:22 +0600

    Server Side Validation.

--- Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -23,9 +23,18 @@ namespace AbbyWeb.Pages.Categories
 
         public async Task<IActionResult> OnPost()
         {
-            await _db.Category.AddAsync(Category);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("Index");
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                await _db.Category.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+            
         }
 
 
